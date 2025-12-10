fe0a27740cce689a0a1a2f9fff167c54acf679e9
Thomas YIu <tom.tyiu@gmail.com>
2025-12-10 10:12:21 -0800

    Create compute_squares.py

---
+++ functions/compute_squares.py
@@ -0,0 +1,6 @@
+def compute_squares(nums):
+    append = list.append  # local reference to speed up method lookup
+    result = []
+    for n in nums:
+        append(result, n * n)
+    return result
