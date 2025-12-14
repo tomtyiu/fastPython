eff3160bd53107cf3e550b3e14b538655026127d
Thomas YIu <tom.tyiu@gmail.com>
2025-12-13 22:23:48 -0800

    Update fast-OpenAI-API.py

--- AI/OpenAI/fast-OpenAI-API.py
+++ AI/OpenAI/fast-OpenAI-API.py
@@ -6,25 +6,14 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-create = client.responses.create
+def OpenAIAPI(prompt):
+    create = client.responses.create
+    responses = create(
+        model="gpt-5.2",
+        instructions="You are a coding assistant that talks like a pirate.",
+        input=prompt,
+    )
 
-print("Normal API code")
-response = client.responses.create(
-    model="gpt-5.2",
-    instructions="You are a coding assistant that talks like a pirate.",
-    input="How do I check if a Python object is an instance of a class?",
-)
-
-print(response.output_text)
-
-print("Fast API code")
-
-create = client.responses.create
-responses = create(
-    model="gpt-5.2",
-    instructions="You are a coding assistant that talks like a pirate.",
-    input="How do I check if a Python object is an instance of a class?",
-)
-
-responses=response.output_text
-print(responses)
+#print out response
+# responses=response.output_text
+# print(responses)
