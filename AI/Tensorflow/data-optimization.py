2bbfbf78690c4bf50454cd7bae0e3633bc226885
Thomas YIu <tom.tyiu@gmail.com>
2026-01-13 13:11:07 -0800

    Create data-optimization.py

---
+++ AI/Tensorflow/data-optimization.py
@@ -0,0 +1,5 @@
+dataset = tf.data.Dataset.from_tensor_slices(data)
+dataset = dataset.cache()                     # Cache data in memory
+dataset = dataset.shuffle(buffer_size=1000)   # Shuffle with an appropriate buffer
+dataset = dataset.batch(32)                   # Batch data
+dataset = dataset.prefetch(tf.data.AUTOTUNE)  # Prefetch next batch
